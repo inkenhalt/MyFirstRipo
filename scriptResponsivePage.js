054fa9ba1d49fded1eed303fd575a08eeac0b097
inkenhalt <maerz.inken@web.de>
2019-08-22 14:25:35 +0200

    Create scriptResponsivePage.js

---
+++ scriptResponsivePage.js
@@ -0,0 +1,30 @@
+alert("Hey there I came from external js file");
+console.log("Hey there am in your browser in console tap")
+/*console.log ("h;gi;a")
+console.log ("h;gi;a")
+console.log ("h;gi;a")
+console.log ("h;gi;a")*/
+console.log(555*999+6-9+545)
+console.log(x) // i dont know what x is until now
+var x=9.5;
+z=10; //not allowed
+var y=5;
+var binary=0b10101010; //how to define a number in binary
+var hexNumber= 0xA //to define hex numbers
+console.log("0xA :"+hexNumber);
+//console.log(here you can put any thing to display)
+console.log(binary+hexNumber);
+var str="1010101010346345";
+y=x;
+var h04mn=1 // no numbers first when u define a variable
+var CAP=5
+var cap=10 //javascript is sensitive to upper-lowercase
+var s="Sehr geehrte Damen und Herren";
+console.log(str);
+var firstNumber= 5;
+var secondNumber= 7;
+var thirdNumber= 10;
+var avr=(firstNumber+secondNumber+thirdNumber)/3
+console.log("The average is : "+avr)
+var firstNumber=555; //not allowed cause already definded
+firstNumber=10; //allowed that is assigning a new value
